5f3bcf1b48f47fd0f5971fbb690e0c32fe1a0fba
yu.ma <my_joy1@qq.com>
2022-09-14 23:26:06 +0800

    finish the day-01 homework

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  var task: [String] = [
+      "晨练",
+      "背单词",
+      "完成作业"]
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+      configTableView()
   }
 
-
+  private func configTableView(){
+    let tableView = UITableView()
+        tableView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: view.bounds.height)
+        self.view.addSubview(tableView)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        tableView.dataSource = self
+  }
 }
+extension ViewController: UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section : Int) -> Int{
+        return task.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "UITableViewCell")!
+        cell.textLabel?.text = task[indexPath.row]
+        return cell
+    }
+}
+
 
